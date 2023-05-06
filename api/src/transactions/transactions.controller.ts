8cbdf1dc9cce79482e440e760c01d3bb82b3a36b
theusindabike <mlopes.matheus@gmail.com>
2023-05-06 16:30:02 -0300

    remove unused files

--- api/src/transactions/transactions.controller.ts
+++ api/src/transactions/transactions.controller.ts
@@ -3,9 +3,6 @@ import {
   Get,
   Post,
   Body,
-  Patch,
-  Param,
-  Delete,
   UseInterceptors,
   UploadedFile,
   ParseFilePipeBuilder,
@@ -15,7 +12,6 @@ import {
 } from '@nestjs/common';
 import { TransactionsService } from './transactions.service';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
-import { UpdateTransactionDto } from './dto/update-transaction.dto';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { TransactionFileDto } from './dto/transaction-file.dto';
 import { ApiBody, ApiConsumes } from '@nestjs/swagger';
